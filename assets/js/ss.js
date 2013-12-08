fcbb90c3c6c40a43f59940182037eb09f78781fd
Carmelyne Thompson <cbthomps@gmail.com>
2013-12-07 22:24:09 -0800

    minor ui tweak for missing shipping icon

--- assets/js/ss.js
+++ assets/js/ss.js
@@ -70,6 +70,7 @@ $(function(){
 
 });
 
+// display wishlist
 function displayContent(json) {
 
     var wish_list_html='';
@@ -79,6 +80,7 @@ function displayContent(json) {
 
       var link = json.feed.entry[i].gsx$googleplusprofilelink.$t;
 
+      // TODO: Carm find out how to resolve vanityURL to profileID
       var profileId = null;
         if (link) {
           var result = /(\d{4,})/.exec(link);
@@ -100,6 +102,7 @@ function displayContent(json) {
       json.feed.entry[i].gsx$amazonwishlist.$t,
       '">',
       'View Wishlist</a>',
+      badAmazon(json.feed.entry[i]),
       getGiftStatus(json.feed.entry[i]),
       '<p>',
       '<img class="profilepic" src="https://plus.google.com/s2/photos/profile/',profileId,'?sz=50"></img></li>',
@@ -121,54 +124,6 @@ function displayContent(json) {
 
 }
 
-function displayFeatured(json) {
-
-    var featured_list_html='';
-    var len = json.feed.entry.length
-
-    for (var i=0; i<len; i++) {
-
-      var link = json.feed.entry[i].gsx$googleplusprofilelink.$t;
-
-      var profileId = null;
-        if (link) {
-          var result = /(\d{4,})/.exec(link);
-          if (result) {
-            profileId = result[0]
-            }
-          } else {
-          profileId="113269791493257695508";
-    }
-
-    featured_list_html += [
-      '<div class="gift-box item">',
-      '<p class="gift-box-header">',
-      '<i class="icon-gift icon-white"><span class="number">',profileId,'</span></i> ',
-      '<a target="_blank" class="" title="Wish List" href="',
-      json.feed.entry[i].gsx$amazonwishlist.$t.trim(),
-      '">',
-      'View Wishlist</a>',
-      getGiftStatus(json.feed.entry[i]),
-      '<p>',
-      '<img class="profilepic" src="https://plus.google.com/s2/photos/profile/',profileId,'?sz=50"></img></li>',
-      '<a class="user name" target="_blank" title="',
-      json.feed.entry[i].gsx$name.$t,
-      '" href="',
-      json.feed.entry[i].gsx$googleplusprofilelink.$t.trim(),
-      '">',
-      json.feed.entry[i].gsx$name.$t,
-      '</a>',
-      '<p class="thoughts">',
-      json.feed.entry[i].gsx$whyisitimportanttoyouthatsecretsantahelpsoutthisyear.$t,
-      '<p>',
-      '</div>'
-      ].join('');
-    }
-
-    document.getElementById('featured_list_html').innerHTML = featured_list_html
-
-}
-
 function getGiftStatus(entry) {
     var iconString = '<span class="pull-right">';
 
@@ -187,13 +142,27 @@ function getGiftStatus(entry) {
     return iconString + '</span>';
 }
 
+function badAmazon(entry) {
+    var iconBadAmazon = ' <span class="no-address pull-right">';
+
+    if ( entry.gsx$incorrectamazonpermalink.$t == "No Address" ) { // has an incorrect Amazon link
+      iconBadAmazon += '<img src="./assets/img/ss-missing-shipping.png" title="Missing Shipping address!" /> ';
+    }
+
+    if ( entry.gsx$incorrectamazonpermalink.$t == "Bad Link" ) { // has an incorrect Amazon link
+      iconBadAmazon += '<img src="./assets/img/ss-bad-amazon-link.png" title="Incorrect Amazon Link!" /> ';
+    }
+
+    return iconBadAmazon + '</span>';
+}
+
 function getRecipient(entry) {
     var addedRecipient;
 
     if ( entry.gsx$giftreceivedorgiftincoming.$t.length > 0 ) {
       addedRecipient = " recipient";
     }
-    return addedRecipient;;
+    return addedRecipient;
 }
 
 function getSanta(entry) {
